5a0fb51618881c640494c703f758400519bbf00d
jacquibushfi <info@stainedglassofmckinney.com>
2020-03-04 07:44:44 -0600

    Done.

--- src/components/QuoteForm.js
+++ src/components/QuoteForm.js
@@ -3,6 +3,7 @@ import uuid from 'uuid';
 import { connect } from 'react-redux';
 import { addQuote } from '../actions/quotes';
 
+
 class QuoteForm extends Component {
 
   state = {
@@ -34,7 +35,7 @@ class QuoteForm extends Component {
           <div className="col-md-8 col-md-offset-2">
             <div className="panel panel-default">
               <div className="panel-body">
-                <form className="form-horizontal">
+                <form className="form-horizontal" onSubmit={this.handleOnSubmit}>
                   <div className="form-group">
                     <label htmlFor="content" className="col-md-4 control-label">Quote</label>
                     <div className="col-md-5">
@@ -73,5 +74,4 @@ class QuoteForm extends Component {
   }
 }
 
-//add arguments to connect as needed
 export default connect(null, { addQuote })(QuoteForm);
